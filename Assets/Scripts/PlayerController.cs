08c431da64d8c580e24a47dc06b03957c54381f8
alexey-pasevich <a.pasevich@carx-tech.com>
2024-10-18 17:43:48 +0300

    Unput Sys

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace TestTaskProj
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private InputActionAsset m_inputActionAsset;
+        [SerializeField] private CharacterController m_characterController;
+
+        private InputActionMap m_playerMap;
+        private InputAction m_moveAction;
+
+        private void Awake()
+        {
+            m_playerMap = m_inputActionAsset.FindActionMap("Player");
+            m_moveAction = m_playerMap.FindAction("Move");
+        }
+        private void OnEnable()
+        { 
+            m_playerMap.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_playerMap.Disable();
+        }
+
+        private void Update()
+        {
+            Vector2 move = m_moveAction.ReadValue<Vector2>();
+            if (move != Vector2.zero) 
+            {
+                Debug.Log(move);
+            }
+        }
+
+    }
+}
